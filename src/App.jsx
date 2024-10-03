341a91b354bcb90931e23222373982bc49447c83
Sandeep <elsenothing321@gmail.com>
2024-10-03 10:14:20 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Button } from "./components/ui/button"
 import { Calendar } from "./components/ui/calendar"
+import { Input } from "./components/ui/input"
 
 function App() {
 
   return (
-    <div className="">
+    <div className="px-4">
       <div className="text-white">Hello</div>
       <div>
         <Button>Hello</Button>
         <Calendar />
+        <Input />
       </div>
     </div>
   )
