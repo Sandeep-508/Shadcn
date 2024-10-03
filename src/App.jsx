850167aeeff6957f3086e82dfcc74d07b45a6e50
Sandeep <elsenothing321@gmail.com>
2024-10-03 10:10:29 +0530

    added shadcn

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+import { Calendar } from "./components/ui/calendar"
+
+function App() {
+
+  return (
+    <div className="">
+      <div className="text-white">Hello</div>
+      <div>
+        <Button>Hello</Button>
+        <Calendar />
+      </div>
+    </div>
+  )
+}
+
+export default App
